93ce928a3e27b660b3458806de8a44de8c6ba0b8
asteroid-612 <ekfqlc0001@kookmin.ac.kr>
2018-05-06 20:52:47 +0900

    login php file upload, but not working.

---
+++ login/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+</head>
+<body>
+    <?php
+    $id = $_GET("school_num");
+    $pw = $_GET("pw");
+    if($id=="20171666" || $id=="20171613" || $id=="20171674" || $id=="20171646" || $id=="20171640"){
+        if($pw=="pw"){
+            echo "로그인 성공!";
+        }
+        else{
+            echo "로그인 실패!";
+        }
+    }
+    ?>
+</body>
+</html>
